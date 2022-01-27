78e5df9f3a859ed370c6fac73f1d4cffdbc4cfdc
unknown <valchukyu@gmail.comm>
2022-01-27 14:01:39 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Likes from './Likes';
+import Title from './Title';
+import Comments from './Comments';
+import Spin from './Spin';
+import { useSelector } from 'react-redux';
+
 
 function App() {
+  const error = useSelector(state => state.appReducer.error);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrap">
+        <Spin />
+        <div className="card">
+          {error && (
+            <div className="error-message">
+              {error}
+            </div>
+          )}
+          <div className="card-image">
+            <img src="./sea.jpg" alt="surfing"/>
+            <Title />
+            <Likes />
+          </div>
+          <Comments />
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import thunk from 'redux-thunk';
+import { createStore, compose, applyMiddleware } from 'redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { rootReducer } from './redux/rootReducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(
+    thunk
+  )
+))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
